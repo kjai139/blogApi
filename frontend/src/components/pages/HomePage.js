d1c2790757f2b3f022c84e3c4d2e154ec0d79548
simon y <71997308+kjai139@users.noreply.github.com>
2023-08-05 02:49:37 -0400

    start homepage render

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosInstance from '../../modules/axiosInstance'
+
 
 const HomePage = () => {
+
+    const [blogPosts, setBlogPosts] = useState()
+
+    const [resultMsg, setResultMsg] = useState('')
+
+    const getBlogPosts = async () => {
+        try {
+            const response = await axiosInstance.get('/posts/get', {
+                withCredentials: true
+            })
+
+            setBlogPosts(response.data.blogPosts)
+        } catch (err) {
+            setResultMsg(err.message)
+        }
+    }
+
+
     return (
         <div>
+            <button>Get Posts</button>
             Home page
         </div>
     )
