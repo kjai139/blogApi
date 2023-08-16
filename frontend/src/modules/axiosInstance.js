3b7ea6831bd824fd932226adf32a64062c8addbc
simon y <71997308+kjai139@users.noreply.github.com>
2023-08-16 15:05:11 -0400

    add vercel

--- frontend/src/modules/axiosInstance.js
+++ frontend/src/modules/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 let baseURL
 if (process.env.NODE_ENV === 'production') {
-    baseURL = ''
+    baseURL = 'https://blog-api-p3ss.vercel.app/'
 } else {
     baseURL = 'http://localhost:4000'
 }
